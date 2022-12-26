57cdb35dc8d1f01b3f7714ca553d52dbf0eb043a
devLucasMoraes <devlucasmoraes@gmail.com>
2022-12-26 11:22:33 -0300

    first commit

---
+++ src/test/java/br/com/devlucasmoraes/java/backend/userapi/UserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devlucasmoraes.java.backend.userapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
